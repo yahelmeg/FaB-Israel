6ee98130b3819a418326edeb240d1a8a7e732443
Yahel Megidish <megidishyahel@gmail.com>
2026-06-17 04:18:48 +0300

    refactor(ui) - change to use nextjs built in image component.

--- components/listings/listing-card.tsx
+++ components/listings/listing-card.tsx
@@ -1,19 +1,26 @@
+import Image from "next/image"
 import { Card, CardContent } from "@/components/ui/card"
 import { Listing } from "@/types/Listing"
 import { ListingCardFooter } from "./listing-card-footer"
 
 interface ListingCardProps {
     listing: Listing;
+    onClick: () => void;
 }
 
-export function ListingCard({ listing }: ListingCardProps) {
+export function ListingCard({ listing, onClick }: ListingCardProps) {
     return (
-        <Card className="group relative overflow-hidden rounded-xl border shadow-sm p-0 gap-0 cursor-pointer">
-            <div className="relative aspect-[3/4] w-full bg-muted">
-                <img src={listing.image} alt="Card" className="h-full w-full object-cover" loading="lazy" />
+        <Card onClick={onClick} className="group relative overflow-hidden rounded-xl border shadow-sm p-0 gap-0 cursor-pointer">
+            <div className="relative aspect-[3/4] w-full bg-white">
+                <Image
+                    src={listing.image}
+                    alt="Card"
+                    fill
+                    loading="lazy"
+                />
             </div>
-            <CardContent className={`p-0 text-[8px] @card-xs:text-[9px] @card-sm:text-[10px] @card-md:text-[11px] @card-lg:text-[13px]`}>
-                <ListingCardFooter price={listing.price} foiling={listing.foiling} condition={listing.condition} />
+            <CardContent className={`p-0`}>
+            <ListingCardFooter price={listing.price} foiling={listing.foiling} condition={listing.condition} />
             </CardContent>
         </Card>
     )
