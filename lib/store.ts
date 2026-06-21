b16f4bbd23e2cc8b2c024d4b7c6cf57594419cfa
Yahel Megidish <megidishyahel@gmail.com>
2026-06-21 19:08:09 +0300

    feat(events): add Store type and lib file to show location and waze link

---
+++ lib/store.ts
@@ -0,0 +1,13 @@
+
+
+import { Store } from "@/types/Store"
+
+export const storeAddresses: Record<Store, string> = {
+    "Sirolynia": "Shmuel Even Olsvenger St 74, Jerusalem",
+    "Rotemz": "David Saharov St 19, Rishon LeZion",
+}
+
+export const storeWazeLinks: Record<Store, string> = {
+    "Sirolynia": "https://waze.com/ul?q=סירולניה+ירושלים",
+    "Rotemz": "https://waze.com/ul?q=רותמז ראשון לציון",
+}
---
+++ types/Store.ts
@@ -0,0 +1,4 @@
+
+
+
+export type Store = "Rotemz" | "Sirolynia"
