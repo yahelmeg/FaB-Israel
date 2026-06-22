ca7ab3ea6a0ca895efd7899ab15af45bccc94846
Yahel Megidish <megidishyahel@gmail.com>
2026-06-22 18:19:36 +0300

    refactor(stores): change store to be optional, removed addresses record

--- lib/store.ts
+++ lib/store.ts
@@ -7,7 +7,3 @@ export const storeAddresses: Record<Store, string> = {
     "Rotemz": "David Saharov St 19, Rishon LeZion",
 }
 
-export const storeWazeLinks: Record<Store, string> = {
-    "Sirolynia": "https://waze.com/ul?q=סירולניה+ירושלים",
-    "Rotemz": "https://waze.com/ul?q=רותמז ראשון לציון",
-}
--- types/Event.ts
+++ types/Event.ts
@@ -1,13 +1,14 @@
 import {EventFormat} from "@/types/EventFormat";
 import {EventTier} from "@/types/EventTier";
+import {Store} from "@/types/Store";
 
 export interface Event {
     id: string
     title: string
     date: string
     time: string
-    location: string
-    store: string
+    address?: string
+    store?: Store
     format: EventFormat
     tier: EventTier
     register_url?: string
