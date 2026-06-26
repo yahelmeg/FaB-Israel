680e3270c637dc75bfc0cc84537564f86fd93819
Yahel Megidish <megidishyahel@gmail.com>
2026-06-26 06:41:57 +0300

    styling(modal): change modal sizing to improve responsiveness

--- components/market/listings/listing-modal.tsx
+++ components/market/listings/listing-modal.tsx
@@ -12,18 +12,15 @@ interface ListingModalProps {
 export function ListingModal({ listing, onClose }: ListingModalProps) {
     return (
         <Dialog open={listing !== null} onOpenChange={onClose}>
-            <DialogContent showCloseButton={false} maxWidth="sm:max-w-[600px] md:max-w-[700px]" className="max-h-[80vh] overflow-y-auto p-4 md:p-0" initialFocus={false}>
+            <DialogContent showCloseButton={false} maxWidth="sm:max-w-[600px] md:max-w-[700px]" className="w-[95vw] sm:w-[600px] md:w-[700px] h-[450px] md:h-[450px] p-4 md:p-0 overflow-hidden" initialFocus={false}>
                 {listing && (
-                    <div className="flex flex-col md:flex-row gap-3">
-                        <div className="hidden md:block relative w-full sm:w-80 aspect-[3/4] shrink-0 bg-white dark:bg-black">
-                            <Image
-                                src={listing.image}
-                                alt={listing.cardName}
-                                fill
-                                sizes="(max-width: 640px) 100vw, 360px"
-                            />
+                    <div className="flex flex-col md:flex-row gap-3 h-full min-h-0">
+                        <div className="hidden md:block relative w-80 h-full shrink-0 bg-white dark:bg-black">
+                            <Image src={listing.image} alt={listing.cardName} fill className="object-cover" />
                         </div>
-                        <ListingModalDetails listing={listing}/>
+                        <div className="flex-1 h-full overflow-y-auto p-0 md:p-6">
+                            <ListingModalDetails listing={listing}/>
+                        </div>
                     </div>
                 )}
             </DialogContent>
