eba74fa110a302d9579e3bca18c236a3b21d201d
Yahel Megidish <megidishyahel@gmail.com>
2026-06-26 06:20:49 +0300

    feat(dialog): add maxWidth prop to DialogContent

--- components/market/listings/listing-modal.tsx
+++ components/market/listings/listing-modal.tsx
@@ -12,7 +12,7 @@ interface ListingModalProps {
 export function ListingModal({ listing, onClose }: ListingModalProps) {
     return (
         <Dialog open={listing !== null} onOpenChange={onClose}>
-            <DialogContent showCloseButton={false} className="!w-[90vw] sm:!w-[600px] md:!w-[700px] !max-w-none !max-h-[80vh] overflow-y-auto p-4 md:p-0" initialFocus={false}>
+            <DialogContent showCloseButton={false} maxWidth="sm:max-w-[600px] md:max-w-[700px]" className="max-h-[80vh] overflow-y-auto p-4 md:p-0" initialFocus={false}>
                 {listing && (
                     <div className="flex flex-col md:flex-row gap-3">
                         <div className="hidden md:block relative w-full sm:w-80 aspect-[3/4] shrink-0 bg-white dark:bg-black">
