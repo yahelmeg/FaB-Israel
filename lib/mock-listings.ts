9a509a1a03fd9246d92f2e08b084f468d9bdef6f
Yahel Megidish <megidishyahel@gmail.com>
2026-06-28 22:08:37 +0300

    test(mock-listings):
    change prices to test for sizing

--- lib/mock-listings.ts
+++ lib/mock-listings.ts
@@ -3,7 +3,7 @@ import {Listing} from "@/types/Listing";
 export const MOCK_LISTINGS: Listing[] = [
     {
         id: "1",
-        price: 125.00,
+        price: 1250.00,
         image: "/cards/IRA001-P_EUmBrk0.width-450.webp",
         condition: "MT",
         foiling: "GF",
