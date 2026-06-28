8287ef0fb905354cf62bd77aacc1654aa65eaf81
Yahel Megidish <megidishyahel@gmail.com>
2026-06-28 22:11:04 +0300

    styling(card-listing-grid):
    change amount of columns appearing in mobile

--- components/market/listings/listing-grid.tsx
+++ components/market/listings/listing-grid.tsx
@@ -13,7 +13,7 @@ export function ListingGrid({listings}: ListingGridProps) {
     const [selectedListing, setSelectedListing] = useState<Listing | null>(null);
     return (
         <>
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
                 {listings.map((listing) => (
                     <ListingCard key={listing.id} listing={listing} onClick={() => setSelectedListing(listing)}/>
                 ))}
