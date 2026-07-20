e5db2dadeadcc802cc82a14b7c565157e3f800bb
Yahel Megidish <megidishyahel@gmail.com>
2026-07-21 01:07:21 +0300

    feat(events): show create event button only to admins

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,14 +1,21 @@
 import {EventGrid} from "@/components/events/event-list";
+import {isAdmin} from "@/lib/auth-helpers/is-admin";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
-
-
-export default function EventsPage() {
+export default async function EventsPage() {
+    const userIsAdmin = await isAdmin();
     return (
         <div className="page-layout">
-            <h2 className="page-heading-text">
-                Upcoming Events
-            </h2>
-            <EventGrid/>
+            <div className="flex items-center justify-between mb-8">
+                <h2 className="page-heading-text">
+                    Upcoming Events
+                </h2>
+            </div>
+            <EventGrid />
+            {userIsAdmin && (
+                <Button className="cursor-pointer" nativeButton={false} render={<Link href="/events/new">Create Event</Link>} />
+            )}
         </div>
     )
 }
