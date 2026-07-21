336e4502077e7787258224a3b958d59e9edb1dae
Yahel Megidish <megidishyahel@gmail.com>
2026-07-21 18:25:45 +0300

    refactor(languages) - remove unneccesary imports

--- lib/languages.ts
+++ lib/languages.ts
@@ -1,12 +1,6 @@
 
-
 import type { LanguageTypes } from "@/types/LanguageTypes"
-import GB from "country-flag-icons/react/3x2/GB";
-import DE from "country-flag-icons/react/3x2/DE";
-import FR from "country-flag-icons/react/3x2/FR";
-import IT from "country-flag-icons/react/3x2/IT";
-import ES from "country-flag-icons/react/3x2/ES";
-import JP from "country-flag-icons/react/3x2/JP";
+
 
 export const LANGUAGE_COUNTRY_MAP: Record<LanguageTypes, string> = {
     EN: "GB",
