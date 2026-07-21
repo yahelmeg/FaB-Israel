a4986e2c4790c4482a276c9e8031b4f63933094a
Yahel Megidish <megidishyahel@gmail.com>
2026-07-21 18:39:05 +0300

    feat(config): allow remote images from Cloudflare R2 bucket

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "pub-685502d84c2a432aa847dc33baf6aa6b.r2.dev",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
