44373105e840469026f30d42a7041de4c50a5d33
Yahel Megidish <megidishyahel@gmail.com>
2026-08-22 21:25:45 +0300

    feat(events): add pre-release as option to event tier type

--- types/events/EventTier.ts
+++ types/events/EventTier.ts
@@ -1,2 +1,2 @@
-export const EVENT_TIERS = ["On Demand", "Play Anywhere", "Armory", "Skirmish", "Nationals", "World Championship Qualifiers"] as const
+export const EVENT_TIERS = ["On Demand", "Play Anywhere", "Armory", "Pre-Release", "Skirmish", "Nationals", "World Championship Qualifiers"] as const
 export type EventTier = typeof EVENT_TIERS[number]
