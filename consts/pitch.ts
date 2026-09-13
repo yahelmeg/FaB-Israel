48f5d2d0aef44d64ae53ffdd2ebf6b4077b55311
Yahel Megidish <megidishyahel@gmail.com>
2026-09-13 14:53:09 +0300

    feat(consts): add pitch value 4 to pitch consts

--- consts/pitch.ts
+++ consts/pitch.ts
@@ -2,10 +2,12 @@ export const PITCH_COLORS: Record<number, string> = {
     1: "text-red-500",
     2: "text-yellow-500",
     3: "text-blue-500",
+    4: "text-purple-500",
 }
 
 export const PITCH_LETTER: Record<number, string> = {
     1: "(R)",
     2: "(Y)",
     3: "(B)",
+    4: "(P)"
 }
